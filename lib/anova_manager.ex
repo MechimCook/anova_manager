951c8b016c48bd387253bf8443b3b857f6b8eccc
mechim.cook20@gmail.com <mechim.cook20@gmail.com>
2025-09-15 22:37:37 -0400

    Init

---
+++ lib/anova_manager.ex
@@ -0,0 +1,18 @@
+defmodule AnovaManager do
+  @moduledoc """
+  Documentation for `AnovaManager`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AnovaManager.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/anova_manager_test.exs
@@ -0,0 +1,8 @@
+defmodule AnovaManagerTest do
+  use ExUnit.Case
+  doctest AnovaManager
+
+  test "greets the world" do
+    assert AnovaManager.hello() == :world
+  end
+end
