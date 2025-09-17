a25268db1de80fefddc9eaab9aea4c341a899dac
mechim.cook20@gmail.com <mechim.cook20@gmail.com>
2025-09-17 11:34:12 -0400

    set apc list on connect

--- lib/anova_manager/anova_sous_vide.ex
+++ lib/anova_manager/anova_sous_vide.ex
@@ -6,7 +6,7 @@ defmodule AnovaWebSocket do
   def connect(token) do
     uri = "#{@base_url}?token=#{token}&supportedAccessories=APC,APO"
 
-    case WebSockex.start_link(uri, __MODULE__, %{discovered?: false}, name: __MODULE__) do
+    case WebSockex.start_link(uri, __MODULE__, %{EVENT_APC_WIFI_LIST: []}, name: __MODULE__) do
       {:ok, pid} ->
         wait_for_device_discovery()
         {:ok, pid}
@@ -16,6 +16,34 @@ defmodule AnovaWebSocket do
         {:error, reason}
     end
   end
+
+  def disconect() do
+    WebSockex.cast(__MODULE__, :close)
+  end
+
+  def handle_frame({:text, msg}, state) do
+    with {:ok, msg_map} <- JSON.decode(msg) do
+      state =
+      case Map.get(msg_map, "command") do
+      "EVENT_APC_WIFI_LIST" ->
+        %{state | EVENT_APC_WIFI_LIST: Map.get(msg_map, "payload")}
+      _ ->
+        state
+      end
+      {:ok, state}
+    else
+        {:error, _} -> IO.puts("⚠️ Failed to decode message: #{msg}")
+          {:ok, state}
+    end
+  end
+
+  def
+
+  def handle_cast(:close, state) do
+    IO.puts("🔌 Disconnecting from server")
+    {:close, state}
+  end
+
 
   defp wait_for_device_discovery(timeout \\ 5_000) do
     receive do
